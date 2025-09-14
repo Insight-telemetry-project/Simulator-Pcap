6b2d8a64a75e265c3f90cf7410ae17fb3986158a
nadav5 <nadavbar205@gmail.com>
2025-09-14 19:15:54 +0300

    [IMPROVE] Work in bits

--- SendRecieveUDP/Service/BitManipulation/BitManipulator.cs
+++ SendRecieveUDP/Service/BitManipulation/BitManipulator.cs
@@ -1,40 +1,38 @@
 ﻿using SendRecieveUDP.Common.Constant;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SendRecieveUDP.Model.Interfaces.BitManipulation;
+
 namespace SendRecieveUDP.Service.BitManipulation
 {
     public class BitManipulator : IBitManipulator
     {
         public void WriteBits(byte[] buffer, int bitOffset, int bitCount, ulong value)
         {
-            for (int indexInByte = ConstantBits.ZERO; indexInByte < bitCount; indexInByte++)
+            for (int indexInByte = 0; indexInByte < bitCount; indexInByte++)
             {
-                int bitVal = (int)(value >> indexInByte & ConstantTypes.ONE_LONG_SIZE);
+                // נחשב לאיזה בייט ולביט בתוך הבייט לכתוב
                 int byteIdx = (bitOffset + indexInByte) / ConstantBits.BITS_IN_BYTE;
-                int bitIdx = (bitOffset + indexInByte) % ConstantBits.BITS_IN_BYTE;
+                int bitIdx = 7 - ((bitOffset + indexInByte) % ConstantBits.BITS_IN_BYTE); // MSB-first
 
-                if (bitVal == ConstantBits.BYTE_SIZE)
-                    buffer[byteIdx] |= (byte)(ConstantBits.BYTE_SIZE << bitIdx);
+                // הביט מתוך value – לוקחים משמאל לימין
+                int bitVal = (int)((value >> (bitCount - 1 - indexInByte)) & 1);
+
+                if (bitVal == 1)
+                    buffer[byteIdx] |= (byte)(1 << bitIdx);
                 else
-                    buffer[byteIdx] &= (byte)~(ConstantBits.BYTE_SIZE << bitIdx);
+                    buffer[byteIdx] &= (byte)~(1 << bitIdx);
             }
         }
 
-
         public ulong ReadBits(byte[] buffer, int bitOffset, int bitCount)
         {
-
-            ulong value = ConstantTypes.ZERO_LONG_SIZE;
-            for (int indexInByte = ConstantBits.ZERO; indexInByte < bitCount; indexInByte++)
+            ulong value = 0;
+            for (int indexInByte = 0; indexInByte < bitCount; indexInByte++)
             {
                 int byteIdx = (bitOffset + indexInByte) / ConstantBits.BITS_IN_BYTE;
-                int bitIdx = (bitOffset + indexInByte) % ConstantBits.BITS_IN_BYTE;
-                int bit = buffer[byteIdx] >> bitIdx & ConstantBits.BYTE_SIZE;
-                value |= (ulong)bit << indexInByte;
+                int bitIdx = 7 - ((bitOffset + indexInByte) % ConstantBits.BITS_IN_BYTE); // MSB-first
+
+                int bit = (buffer[byteIdx] >> bitIdx) & 1;
+                value = (value << 1) | (ulong)bit; // מכניס משמאל לימין
             }
             return value;
         }
